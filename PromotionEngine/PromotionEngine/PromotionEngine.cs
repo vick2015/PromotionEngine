1e3d5607e8916337f50f8a1d5f95333ddb6c464f
Bhartia, Vikash (HC SI DC IN DI2 CT ONCO) <vikash.bhartia@siemens-healthcare.com>
2021-06-18 16:42:29 +0530

    added Tests along with basic skeleton of PomotionEngine

---
+++ PromotionEngine/PromotionEngine/PromotionEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PromotionEngine.Contracts;
+
+
+namespace PromotionEngine
+{
+    public class PromotionEngine : IPromotionEngine
+    {
+        public double ApplyPromotion()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PromotionEngine/Test/PromotionEngine.Test/PromotionEngineTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Test
+{
+    public class PromotionEngineTests
+    {
+        private PromotionEngine myPromotionEngine;
+
+        [SetUp]
+        public void SetUp()
+        {
+            myPromotionEngine = new PromotionEngine();
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            myPromotionEngine = null;
+        }
+
+        [Test]
+
+        public void ApplyPromotion_WithSingleUnitOfSku_TotalShouldBeReturned()
+        {
+
+        }
+
+        [Test]
+        public void ApplyPromotion_WIthPromotionTypes_DiscountedTotalShouldBeReturned()
+        {
+
+        }
+
+        [Test]
+        public void ApplyPromotion_WithMultiplePromotionTypes_DiscountedTotalShouldBeReturned()
+        {
+
+        }
+
+    }
+}
