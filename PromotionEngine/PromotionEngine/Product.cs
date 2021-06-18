3ea210d75e939e274c5a6a4cbe39337d170286e3
Bhartia, Vikash (HC SI DC IN DI2 CT ONCO) <vikash.bhartia@siemens-healthcare.com>
2021-06-18 16:55:37 +0530

    added product class inside model

---
+++ PromotionEngine/PromotionEngine/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models
+{
+    public class Product
+    {
+        public string SKU { get; }
+
+        public double UnitPrice { get; }
+
+        public Product(string sku  , double unitPrice)
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+        }
+    }
+}
